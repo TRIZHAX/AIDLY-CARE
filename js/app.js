3336754338391bdeb793f6171ba2adfae1e4f2f2
TRIZHAX <anonydarknet@gmail.com>
2025-09-12 15:10:28 +0800

    app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// ======================= CORE APP FUNCTIONS =======================
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js')
     .then(() => console.log("Service Worker registered"))
